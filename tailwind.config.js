58cdfabf7380b7c26ba2d14c5d9430262e052bcd
gabrielelivigni15-cyber <gabrielelivigni15@gmail.com>
2025-12-12 23:04:59 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        nutriBg: "#0B0F14",
+        nutriPrimary: "#B8FF5A",
+        nutriAccent: "#7CFFB2",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 }
