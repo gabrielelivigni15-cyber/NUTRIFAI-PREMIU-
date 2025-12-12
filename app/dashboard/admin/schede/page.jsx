871505c02ef33b603b5ee73d7f8f3ef4b140d42b
gabrielelivigni15-cyber <gabrielelivigni15@gmail.com>
2025-12-12 23:12:48 +0100

    Update page.jsx

--- app/dashboard/admin/schede/page.jsx
+++ app/dashboard/admin/schede/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useMemo, useState } from "react"
-import { supabase } from "@/app/lib/supabaseClient"
+import { supabase } from "../../../lib/supabaseClient"
 
 export default function SchedeAdminPage() {
   const [loading, setLoading] = useState(true)
